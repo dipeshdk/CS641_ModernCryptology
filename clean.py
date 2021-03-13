a84fed2e6f31c7ad84f0e2dc47110f17a8e51278
Dipesh Khandelwal <dipeshmps@gmail.com>
2021-03-13 23:10:52 +0530

    separate files from clean.py

--- clean.py
+++ clean.py
@@ -1,13 +1,13 @@
 import re
 
-outputFileFinal = open("final_outputs_2.txt","a")
+outputFileFinal = open("final_outputs_10.txt","a")
 inputFile = open("outputs10.txt","r")
-count = 0
+# count = 0
 for line in inputFile:
     if re.findall("\t\t",line):
-        count = count + 1
+        # count = count + 1
         outputFileFinal.write(line)
 
-print(f"{inputFile} pushed {count} lines in {outputFileFinal}")
+# print(f"{inputFile} pushed {count} lines in {outputFileFinal}")
 outputFileFinal.close()
 inputFile.close()
