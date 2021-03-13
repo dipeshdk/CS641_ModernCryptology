f28697687d940a72c429ace8c70aaee6904354fc
Dipesh Khandelwal <dipeshmps@gmail.com>
2021-03-13 23:02:55 +0530

    final_outputs merged according to ch.eq.

--- clean.py
+++ clean.py
@@ -1,10 +1,13 @@
 import re
 
-outputFileFinal = open("final_outputs_2.txt","w+")
-inputFile = open("outputs.txt","r")
+outputFileFinal = open("final_outputs_2.txt","a")
+inputFile = open("outputs10.txt","r")
+count = 0
 for line in inputFile:
     if re.findall("\t\t",line):
+        count = count + 1
         outputFileFinal.write(line)
 
+print(f"{inputFile} pushed {count} lines in {outputFileFinal}")
 outputFileFinal.close()
 inputFile.close()
