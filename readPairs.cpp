4fb477b1d868610b8809a98a1e9ea73ca27f39c2
Dipesh Khandelwal <dipeshmps@gmail.com>
2021-03-13 20:43:54 +0530

    output for github repo generated

--- readPairs.cpp
+++ readPairs.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     ifstream fin;
-    fin.open("final_outputs_1.txt");
+    fin.open("final_outputs_2.txt");
     string line;
     string cipherPairs[320][2];
     if(!fin) return 0;
@@ -17,9 +17,13 @@ int main(){
     }
     fin.close();
 
+    ofstream fout;
+    fout.open("output_2.txt");
+
     // the following code if for verification purpose only 
-    // for(int i = 0 ; i < 320; i++){
-    //     cout << i << " " << cipherPairs[i][0] << "  " << cipherPairs[i][1] << endl;
-    // }
+    for(int i = 0 ; i < 320; i++){
+        fout << cipherPairs[i][0] << " " << cipherPairs[i][1] << endl;
+    }
+    fout.close();
     return 0;
 }
